0fb03d4955e8fa55a5b760d7103fdf7ec4176da2
Radek Molenda <radek.molenda@onthebeach.co.uk>
2014-02-28 22:41:20 +0000

    Add Event model

---
+++ app/models/event.rb
@@ -0,0 +1,20 @@
+class Event
+  include Id::Model
+
+  field :id
+  field :is_virtual
+  field :event_id
+  field :title
+  field :score
+  field :start_time
+  field :meeting
+  field :meeting_id
+  field :american_format
+  field :event_type
+  field :pos
+  field :status
+  field :status_type
+  field :total_outcomes
+  has_many :outcomes
+
+end
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'models/event'
+
+describe Event do
+  let(:data) {
+    {
+      id: 44017910,
+      is_virtual: false,
+      outcomes: [],
+      event_id: 470252100,
+      title: "Athletic Bilbao v Granada CF",
+      score: "4 - 0",
+      start_time: 1393615800000,
+      meeting: "ESP La Liga Primera",
+      meeting_id: 6320610,
+      american_format: false,
+      event_type: "GAMEEVENT",
+      pos: 10,
+      status: "Second Half",
+      status_type: "text",
+      total_outcomes: 0
+    }
+  }
+
+  subject { described_class.new(data) }
+
+  describe "attributes" do
+    it { expect(subject.id).to eq(44017910) }
+    it { expect(subject.is_virtual).to eq(false) }
+    it { expect(subject.outcomes).to eq([]) }
+    it { expect(subject.event_id).to eq(470252100) }
+    it { expect(subject.title).to eq("Athletic Bilbao v Granada CF") }
+    it { expect(subject.score).to eq("4 - 0") }
+    it { expect(subject.start_time).to eq(1393615800000) }
+    it { expect(subject.meeting).to eq("ESP La Liga Primera") }
+    it { expect(subject.meeting_id).to eq(6320610) }
+    it { expect(subject.american_format).to eq(false) }
+    it { expect(subject.event_type).to eq("GAMEEVENT") }
+    it { expect(subject.pos).to eq(10) }
+    it { expect(subject.status).to eq("Second Half") }
+    it { expect(subject.status_type).to eq("text") }
+    it { expect(subject.total_outcomes).to eq(0) }
+  end
+end
