d34f5d98add2aa6521357f75d90aed19acd2c59f
Radek Molenda <radek.molenda@onthebeach.co.uk>
2014-03-02 16:40:19 +0000

    Use sport_list only

--- app.rb
+++ app.rb
@@ -24,25 +24,16 @@ class Application < Sinatra::Base
 
   get '/sports' do
     @sport_list = SportListPresenter.new(SportList.fetch(I18n.locale), params)
-    @sports = @sport_list.sports
     erb :sports, locals: { active: -1 }
   end
 
   get '/sports/:sport_id' do
     @sport_list = SportListPresenter.new(SportList.fetch(I18n.locale), params)
-    @sports = @sport_list.sports
-    @events = @sport_list.events
-    @sport = @sport_list.sport
     erb :sport, locals: { active: -1 }
   end
 
   get '/sports/:sport_id/events/:event_id' do
     @sport_list = SportListPresenter.new(SportList.fetch(I18n.locale), params)
-    @sports = @sport_list.sports
-    @event = @sport_list.event
-    @sport = @sport_list.sport
-    @events = @sport_list.events
-    @outcomes = @sport_list.outcomes
     erb :outcomes
   end
 end
