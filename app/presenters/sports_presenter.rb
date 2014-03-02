37e56034b3a629ea7288ba6744597f979f22a36f
Radek Molenda <radek.molenda@onthebeach.co.uk>
2014-03-02 17:17:40 +0000

    Introduce SportsPresenter

---
+++ app/presenters/sports_presenter.rb
@@ -0,0 +1,21 @@
+class SportsPresenter
+  def initialize(sport_list, params)
+    @params = params
+    @sport_list = sport_list
+  end
+
+  def sports
+    sport_list.ordered_sports
+  end
+
+  def sport
+    sport_list.find_sport(sport_id)
+  end
+
+  private
+  attr_reader :sport_list, :params
+
+  def sport_id
+    @sport_id ||= params.fetch("sport_id").to_i
+  end
+end
---
+++ spec/presenters/sports_presenter_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe SportsPresenter do
+  let(:sport_list) { double("SportList",
+                            ordered_sports: sports) }
+  let(:presenter) { described_class.new(sport_list, params) }
+  let(:params) { {} }
+  let(:sport) { double("Sport") }
+  let(:sports) { [sport] }
+
+  describe "#sports" do
+    subject { presenter.sports }
+
+    it { expect(subject).to eq(sports) }
+  end
+
+  describe "#sport" do
+    let(:sport_id) { 1 }
+    let(:params) { { "sport_id" => sport_id } }
+
+    before do
+      sport_list.stub(:find_sport).with(sport_id).and_return(sport)
+    end
+
+    subject { presenter.sport }
+
+    it { expect(subject).to eq(sport) }
+  end
+end
